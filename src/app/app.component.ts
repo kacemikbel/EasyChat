40484c793a475357ed49cc8d9266838f46283272
Kacem ikbel <kassemikbel56@gmail.com>
2024-05-30 22:05:09 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component'; 
-
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterOutlet,CommonModule,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'EASYCHATT';
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,12 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'] 
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  userDetails = {
-    email: '',
-    address: ''
-  };
 
-  submitForm(userForm: NgForm): void {
-    if (userForm.valid) {
-      
-      console.log('Form submitted successfully!', this.userDetails);
-      
-      userForm.resetForm();
-    } else {
-      
-      console.log('Form is invalid. Please check your inputs.');
-    }
-  }
 }
