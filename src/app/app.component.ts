80b8a73e34b7571f05b2f314ab9abd5fcb92d9c3
Kacem ikbel <kassemikbel56@gmail.com>
2024-06-01 19:35:57 +0200

    routers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from './login/login.component'
 import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, CommonModule, LoginComponent, FormsModule ],
+  imports: [ RouterOutlet, CommonModule, LoginComponent, FormsModule , SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormComponent } from './login/form/form.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { SignupComponent } from './signup/signup.component';
 
-
+import { routes } from './app.routes'; 
 @NgModule({
   declarations: [
-  AppComponent,
-  LoginComponent,
-  FormComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    CommonModule,
     FormsModule,
-    ReactiveFormsModule
-
-  ]
+    RouterModule.forRoot(routes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
