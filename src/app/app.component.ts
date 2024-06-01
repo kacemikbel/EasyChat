32ceb036d5c141770b374c6dd2e7b191ce99485b
Kacem ikbel <kassemikbel56@gmail.com>
2024-06-01 06:23:09 +0200

    the first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { LogformComponent } from './login/logform/logform.component';
+import {LoginComponent} from './login/login.component'
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterOutlet,CommonModule,LoginComponent,LogformComponent ],
+  imports: [ RouterOutlet, CommonModule, LoginComponent, FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormComponent } from './login/form/form.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  AppComponent,
+  LoginComponent,
+  FormComponent
+  ],
   imports: [
-    ReactiveFormsModule,
-    CommonModuleng 
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
