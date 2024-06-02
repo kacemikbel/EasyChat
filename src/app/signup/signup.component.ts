878d5fa53f6e030daac7ccb58e7952ad64555a5d
Kacem ikbel <kassemikbel56@gmail.com>
2024-06-02 22:43:39 +0200

    Signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -13,6 +13,7 @@ export class SignupComponent {
   SignupForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
+    
 
   });
   onSubmit() {
@@ -23,5 +24,5 @@ export class SignupComponent {
 
 
 
-  
+
 }
